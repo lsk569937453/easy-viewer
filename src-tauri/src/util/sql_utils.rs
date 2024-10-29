7e32dac01b430a8ea9ae1b337b0841d9c38a958c
shikai liu <lsk569937453@gmail.com>
2024-10-29 09:53:53 +0800

    Add the sql utils.

--- src-tauri/src/util/sql_utils.rs
+++ src-tauri/src/util/sql_utils.rs
@@ -1,5 +1,8 @@
+use chrono::{DateTime, Local, NaiveDateTime};
+use chrono::{NaiveDate, NaiveTime};
 use serde_json::{json, Value};
 use sqlx::mysql::MySqlRow;
+use sqlx::types::BigDecimal;
 use sqlx::Row;
 
 pub fn mysql_row_to_json(
@@ -11,15 +14,40 @@ pub fn mysql_row_to_json(
         "FLOAT" => row
             .try_get::<Option<f32>, usize>(key)
             .map(|item| json!(item))?,
+        "BIT" => row
+            .try_get::<Option<bool>, usize>(key)
+            .map(|item| json!(item))?,
+
         "DOUBLE" => row
             .try_get::<Option<f64>, usize>(key)
             .map(|item| json!(item))?,
-        "INT" | "TINYINT" | "SMALLINT" | "MEDIUMINT" => row
+        "DECIMAL" => row
+            .try_get::<Option<BigDecimal>, usize>(key)
+            .map(|item| json!(item))?,
+        "YEAR" | "INT" | "TINYINT" | "SMALLINT" | "MEDIUMINT" => row
             .try_get::<Option<i32>, usize>(key)
             .map(|item| json!(item))?,
-        "DATE" | "DATETIME | TIMESTAMP" => row
-            .try_get::<Option<f64>, usize>(key)
+        "BIGINT" => row
+            .try_get::<Option<i64>, usize>(key)
             .map(|item| json!(item))?,
+
+        "DATE" => row
+            .try_get::<Option<NaiveDate>, usize>(key)
+            .map(|item| json!(item.unwrap_or_default().format("%Y-%m-%d").to_string()))?,
+        "TIMESTAMP" => row
+            .try_get::<Option<DateTime<Local>>, usize>(key)
+            .map(|item| json!(item.unwrap_or_default().format("%Y-%m-%d").to_string()))?,
+        "TIME" => row
+            .try_get::<Option<NaiveTime>, usize>(key)
+            .map(|item| json!(format!("{:?}", item.unwrap_or_default())))?,
+        "DATETIME" => row
+            .try_get::<Option<NaiveDateTime>, usize>(key)
+            .map(|item| {
+                json!(item
+                    .unwrap_or_default()
+                    .format("%Y-%m-%d %H:%M:%S")
+                    .to_string())
+            })?,
         _ => row
             .try_get::<Option<String>, usize>(key)
             .map(|item| json!(item))?,

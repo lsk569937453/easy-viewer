ffb65e54f285a00de574d6afcf3c283cb948cee7
shikai liu <lsk569937453@gmail.com>
2024-10-30 15:29:05 +0800

    Update the README.MD.

--- src/dashboard/components/treeNode.jsx
+++ src/dashboard/components/treeNode.jsx
@@ -143,7 +143,8 @@ const TreeNode = ({ node, style, dragHandle, handleAddPageClick, setCurrentMenuL
             handleAddPageClick({
                 name: "table",
                 icon: <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-border-all"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M4 12l16 0" /><path d="M12 4l0 16" /></svg>,
-                render: <TablePage node={node} />
+                render: <TablePage node={node} />,
+                service: node.data.name,
             })
         }
     }

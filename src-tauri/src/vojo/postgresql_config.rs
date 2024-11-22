e73718033ee2882d89be2d7a70c1391f307e9c45
Shikai Liu <lsk569937453@gmail.com>
2024-11-22 22:29:29 +0800

    Add postgresql .

--- src-tauri/src/vojo/postgresql_config.rs
+++ src-tauri/src/vojo/postgresql_config.rs
@@ -3,11 +3,38 @@ use super::list_node_info_req::ListNodeInfoReq;
 use crate::sql_lite::connection::AppState;
 use crate::vojo::base_config::DatabaseHostStruct;
 use crate::vojo::list_node_info_response::ListNodeInfoResponse;
+use crate::vojo::list_node_info_response::ListNodeInfoResponseItem;
 use crate::vojo::show_column_response::ShowColumnsResponse;
 use anyhow::Ok;
+use linked_hash_map::LinkedHashMap;
 use serde::Deserialize;
 use serde::Serialize;
+use std::sync::OnceLock;
 use tokio_postgres::NoTls;
+static POSTGRESQL_DATABASE_DATA: OnceLock<LinkedHashMap<&'static str, &'static str>> =
+    OnceLock::new();
+static POSTGRESQL_TABLE_DATA: OnceLock<LinkedHashMap<&'static str, &'static str>> = OnceLock::new();
+
+fn get_postgresql_database_data() -> &'static LinkedHashMap<&'static str, &'static str> {
+    POSTGRESQL_DATABASE_DATA.get_or_init(|| {
+        let mut map = LinkedHashMap::new();
+        map.insert("Query", "query");
+        map.insert("Tables", "tables");
+        map.insert("Views", "views");
+        map.insert("Functions", "functions");
+        map.insert("Procedures", "procedures");
+        map
+    })
+}
+fn get_postgresql_table_data() -> &'static LinkedHashMap<&'static str, &'static str> {
+    POSTGRESQL_TABLE_DATA.get_or_init(|| {
+        let mut map = LinkedHashMap::new();
+        map.insert("Columns", "columns");
+        map.insert("Index", "index");
+        map.insert("Partitions", "partitions");
+        map
+    })
+}
 #[derive(Deserialize, Serialize, Clone)]
 pub struct PostgresqlConfig {
     pub config: DatabaseHostStruct,
@@ -45,7 +72,67 @@ impl PostgresqlConfig {
         &self,
         list_node_info_req: ListNodeInfoReq,
     ) -> Result<ListNodeInfoResponse, anyhow::Error> {
-        let test_url = self.config.to_url("mysql".to_string());
+        let mut vec = vec![];
+
+        let test_url = self.connection_url();
+        info!("test_url: {}", test_url);
+        let (client, connection) = tokio_postgres::connect(&test_url, NoTls).await?;
+        tokio::spawn(async move {
+            if let Err(e) = connection.await {
+                error!("connection error: {}", e);
+            }
+        });
+        if list_node_info_req.level_infos.len() == 1 {
+            let rows = client
+                .query(
+                    "SELECT datname FROM pg_database WHERE datistemplate = false;",
+                    &[],
+                )
+                .await?;
+            if rows.is_empty() {
+                return Ok(ListNodeInfoResponse::new_with_empty());
+            }
+            for item in rows.iter() {
+                let database_name: String = item.try_get(0)?;
+                let list_node_info_response_item = ListNodeInfoResponseItem::new(
+                    true,
+                    true,
+                    "database".to_string(),
+                    database_name.to_string(),
+                    None,
+                );
+                vec.push(list_node_info_response_item);
+            }
+            return Ok(ListNodeInfoResponse::new(vec));
+        } else if list_node_info_req.level_infos.len() == 2 {
+            let list_node_info_response_item = ListNodeInfoResponseItem::new(
+                true,
+                true,
+                "public".to_string(),
+                "public".to_string(),
+                None,
+            );
+            vec.push(list_node_info_response_item);
+            return Ok(ListNodeInfoResponse::new(vec));
+        } else if list_node_info_req.level_infos.len() == 3 {
+            for (name, icon_name) in get_postgresql_database_data().iter() {
+                // let description = if *name == "Tables" && tables_count > 0 {
+                //     Some(format!("({})", tables_count))
+                // } else {
+                //     None
+                // };
+                // info!("description: {}", tables_count);
+                let list_node_info_response_item = ListNodeInfoResponseItem::new(
+                    true,
+                    true,
+                    icon_name.to_string(),
+                    name.to_string(),
+                    None,
+                );
+                vec.push(list_node_info_response_item);
+            }
+            return Ok(ListNodeInfoResponse::new(vec));
+        }
         Ok(ListNodeInfoResponse::new_with_empty())
     }
     pub async fn exe_sql(

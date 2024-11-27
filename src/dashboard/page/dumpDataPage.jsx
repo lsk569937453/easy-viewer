f3ce37e888f58d1559436806a2e87785e73bfd16
Shikai Liu <lsk569937453@gmail.com>
2024-11-27 22:17:20 +0800

    Add the dump data page.

--- src/dashboard/page/dumpDataPage.jsx
+++ src/dashboard/page/dumpDataPage.jsx
@@ -2,7 +2,16 @@ import { createContext, useEffect, useRef, useState } from "react"
 import { invoke } from "@tauri-apps/api/core"
 
 import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import { Checkbox } from "@/components/ui/checkbox"
+import { Input } from "@/components/ui/input"
 import {
   Select,
   SelectContent,
@@ -22,7 +31,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import { useToast } from "@/components/hooks/use-toast"
+import { useToast } from "@/components/ui/use-toast"
 
 import { getLevelInfos } from "../../lib/jsx-utils"
 
@@ -57,7 +66,7 @@ const DumpDataPage = ({ node }) => {
     <div className="flex h-full w-full flex-col  gap-2 p-4">
       <div className="max-h-1/2 flex w-full flex-row">
         <div className="flex basis-1/2 flex-col">
-          <Table className="h-full w-full border">
+          <Table className="h-full w-full  border">
             <TableHeader>
               <TableRow>
                 <TableHead>Table Name</TableHead>
@@ -91,25 +100,41 @@ const DumpDataPage = ({ node }) => {
           </Table>
         </div>
       </div>
-      <div className="flex flex-row items-center justify-start">
-        <div>Objects To Export:</div>
-        <Select>
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Select a fruit" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectLabel>Fruits</SelectLabel>
-              <SelectItem value="apple">Apple</SelectItem>
-              <SelectItem value="banana">Banana</SelectItem>
-              <SelectItem value="blueberry">Blueberry</SelectItem>
-              <SelectItem value="grapes">Grapes</SelectItem>
-              <SelectItem value="pineapple">Pineapple</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        <Button variant="outline">Button</Button>
-      </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Export Options</CardTitle>
+        </CardHeader>
+        <CardContent className=" flex flex-col gap-2">
+          <div className="flex w-full flex-row items-center justify-start gap-2 text-right">
+            <div className="basis-1/4">Objects To Export:</div>
+            <Select>
+              <SelectTrigger className="w-[180px] focus:ring-1 focus:ring-offset-0">
+                <SelectValue placeholder="Select a fruit" value="dumapAll" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="dumapAll">
+                    Dump Structure and Data
+                  </SelectItem>
+                  <SelectItem value="dumpData">Dump Data Only</SelectItem>
+                  <SelectItem value="dumpStructure">
+                    Dump Structure Only
+                  </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="medium flex w-full flex-row items-center justify-start gap-2 text-right">
+            <div className="basis-1/4">Export To Self-containered File:</div>
+            <Input className="w-80 focus-visible:ring-1 focus-visible:ring-offset-0" />
+            <Button>.....</Button>
+          </div>
+          <div className="medium flex w-full flex-row items-center justify-start gap-2 text-right">
+            <div className="basis-1/4">Print [Start Export] to start...</div>
+            <Button>Start Export</Button>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   )
 }
